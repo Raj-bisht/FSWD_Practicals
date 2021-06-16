37e469d033a65b3cd0d65dc9f41cc8c6f970196e
Raj-bisht <61263677+Raj-bisht@users.noreply.github.com>
2021-06-16 11:24:30 +0530

    exam

--- End Term Practicals/CSEA/44_2013441_Raj_Bisht/Q1/1.php
+++ End Term Practicals/CSEA/44_2013441_Raj_Bisht/Q1/1.php
@@ -1,94 +1,79 @@
 <?php
-   require_once('config.php');
-
+include_once('config.php');
 ?>
-
 <?php
-$showAlert=false;
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    include('config.php');
-    $bname = $_POST['bname'];
-    $aname = $_POST['aname'];
-    $isbn = $_POST['isbn'];
-    $pname = $_POST['pname'];
-    $sql = "INSERT INTO `users` (`bname`, `aname`, `isbn`, `pname`) VALUES ('$bname', ' $aname', '$isbn', ' $pname')";
-    $result=mysqli_query($conn, $sql);
-    if($result){
-      $showAlert=true;
-      header('location:details.php');
-    }
-      else{
-        echo "there was an error";
-      }
-    }
+if(isset($_POST['submit'])){
+    $bookname = $_POST['bookname'];
+    $authorname = $_POST['authorname'];
+    $isbnnumber = $_POST['isbnnumber'];
+    $publishername = $_POST['publishername'];
+    $publicationyear = $_POST['Year'];
+    $noofpages = $_POST['noofpages'];
+    $booktype = $_POST['booktype'];
+
+    $sql = "INSERT INTO `BookStore` (`id`, 'bookName', 'authorName', 'isbnNumber',
+    'publisherName', 'publicationYear', 'noOfPages', 'bookType') VALUES (NULL, '$bookname', '$authorname', '$isbnnumber', '$publishername','$publicationyear',
+    '$noofpages','$booktype')";
+
+    $sql = "INSERT INTO `bookstore` (`id`, `book_name`, `author_name`, `isbn_num`, `pub_name`, `pub_year`, `no_pages`, `booktype`)
+     VALUES (NULL, '$bookname', '$authorname', '$isbnnumber', '$publishername', '$publicationyear', '$noofpages', '$booktype');";
+     mysqli_query($conn,$sql);
+}
+else{
+    echo "please click submit button";
+}
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book</title>
-
-</head>
-<body>
-<?php
-  if($showAlert)
-  {
-    echo '<div class="alert alert-success" role="alert">
-      Successfully inserted!
-  </div>';
-  }
-?>
-<div class="container">
-      <html>
+<html>
     <head>
         <title>
             Book
         </title>
     </head>
     <body>
-        <form method="POST" action="">
-            Book Name <input type="text" placeholder="Enter Book Name" name="bname"><br><br>
-            Author Name <input type="text" placeholder="Enter Author Name" name="aname"><br><br>
-            ISBN Number <input type="text" placeholder="Enter ISBN Number" name="isbn"><br><br>
-            Publisher Name <input type="text" placeholder="Enter Publisher Name" name="pname"><br><br>
-            <br>Publication Year<br>
-            <select name="year">
-                <option value="2010">2010</option>
-                <option value="2011">2011</option>
-                <option value="2012">2012</option>
-                <option value="2013">2013</option>
-                <option value="2014">2014</option>
-                <option value="2015">2015</option>
-                <option value="2016">2016</option>
-                <option value="2017">2017</option>
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
-              </select><br>
-            <br> Book Type <br>
-            Action <input type="radio" value="Action" name="btype"><br>
-            Adventure <input type="radio" value="Adventure" name="btype"><br>
-            Classics <input type="radio" value="Classics" name="btype"><br>
-            Comic Book <input type="radio" value="Comic_Book" name="btype"><br>
-            Graphic Novel <input type="radio" value="Graphic_Novel" name="btype"><br>
-            Detective <input type="radio" value="Detective" name="btype"><br>
-            Mystery <input type="radio" value="Mystery" name="btype"><br>
-            Historical Fiction <input type="radio" value="Historical_Fiction" name="btype"><br>
-            Horror <input type="radio" value="Horror" name="btype"><br>
-            Literary Fiction <input type="radio" value="Literary_Fiction" name="btype"><br>
-            <br>
-            Number Of Pages<br>
-            <input type="range" name="volume" min="200" max="400"> <br><br>
-            <input type="submit" name="submit">
-        </form>
-                   
-                
-    </div>
     
-   
-    
+    <form action="1.php" method="POST">
+        Enter Book Name:<input type="text" name="bookname"><br><br>
+        Enter author name: <input type="text" name="authorname"><br><br>
+        Enter ISBN Number: <input type="text"name="isbnnumber"><br><br>
+        Enter Publisher Name: <input type="text" name="publishername"><br><br>
+        Enter Publication Year: <select name="Year" id=""><br>
+            <option value="2010">2010</option>
+            <option value="2011">2011</option>
+            <option value="2012">2012/option>
+            <option value="2013">2013</option>
+            <option value="2014">2014</option>
+            <option value="2015">2015</option>
+            <option value="2016">2016</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019<option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+        </select>
+        Number of pages: <input type="number" min="200" max="400" name="noofpages"><br>
+        Book Type:<input type="radio" id="action" name="booktype" value="action">
+        <label for="action">Action</label><br>
+        <input type="radio" id="adventure" name="booktype" value="adventure">
+        <label for="adventure">Adventure</label><br>
+        <input type="radio" id="classics" name="booktype" value="classics">
+        <label for="classics">Classics</label><br>
+        <input type="radio" id="comicbook" name="booktype" value="comicbook">
+        <label for="comicbook">Comic Book</label><br>
+        <input type="radio" id="graphicnovel" name="booktype" value="graphicnovel">
+        <label for="graphicnovel">Graphic Novel</label><br>
+        <input type="radio" id="detective" name="booktype" value="detective">
+        <label for="detective">Detective</label><br>
+        <input type="radio" id="mystery" name="booktype" value="mystery">
+        <label for="mystery">Mystery</label><br>
+        <input type="radio" id="hfiction" name="booktype" value="historical fiction">
+        <label for="historical fiction">Historical Fiction</label><br>
+        <input type="radio" id="horror" name="booktype" value="horror">
+        <label for="horror">Horror</label><br>
+        <input type="radio" id="lfiction" name="booktype" value="literary fiction">
+        <label for="literary fiction">Literary Fiction</label><br>
+        <input type="submit" name="submit"><br>
+    </form>          
+      
 </body>
 </html>
